a095639ba10f263bda05c09d465b11a47e0a1a44
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-13 00:20:36 +0300

    Call convertFile from main

--- src/main/java/gr/europeandynamics/xmlmanagement/XmlManagement.java
+++ src/main/java/gr/europeandynamics/xmlmanagement/XmlManagement.java
@@ -1,15 +1,15 @@
 package gr.europeandynamics.xmlmanagement;
 
-import gr.europeandynamics.xmlmanagement.importfileservice.TXTParser;
+import gr.europeandynamics.xmlmanagement.fileconvertorservice.ConvertTXTToXML;
 import java.io.IOException;
 
 public class XmlManagement {
 
     public static void main(String[] args) throws IOException {
 
-        TXTParser txtParser = new TXTParser();
+        ConvertTXTToXML txtParser = new ConvertTXTToXML();
 
-        txtParser.readFile("txt_files/sample-lorem-ipsum-text-file.txt");
+        txtParser.convertFile("txt_files/sample-lorem-ipsum-text-file.txt","xml_files/book.xml");
     }
 
 }
