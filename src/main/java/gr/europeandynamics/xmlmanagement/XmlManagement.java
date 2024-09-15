afd7198903a23d39ce587ee4166e010df9f3f95c
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-15 22:02:28 +0300

    Update XmlManagement.java

--- src/main/java/gr/europeandynamics/xmlmanagement/XmlManagement.java
+++ src/main/java/gr/europeandynamics/xmlmanagement/XmlManagement.java
@@ -1,6 +1,8 @@
 package gr.europeandynamics.xmlmanagement;
 
 import gr.europeandynamics.xmlmanagement.fileconvertorservice.FileConvertorImpl;
+import gr.europeandynamics.xmlmanagement.xmlmanipulationservice.XmlValidator;
+import gr.europeandynamics.xmlmanagement.xmlmanipulationservice.XsdGenerator;
 
 import java.io.IOException;
 
@@ -11,6 +13,9 @@ public class XmlManagement {
         FileConvertorImpl fileConvertor = new FileConvertorImpl();
 
         fileConvertor.convertFile("txt_files/sample-lorem-ipsum-text-file.txt", "xml_files/book.xml");
-
+        final String xsdFileName = "xml_files/book-schema.xsd";
+        XsdGenerator.xsdGenerator(xsdFileName);
+        final String xmlFileName = "xml_files/book.xml";
+        XmlValidator.xmlValidator(xmlFileName, xsdFileName);
     }
 }
