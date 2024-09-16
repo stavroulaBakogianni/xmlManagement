61e0473a15d9095f80e0ba93a417223b07058c51
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-16 21:13:42 +0300

    Create XmlManipulator Interface

---
+++ src/main/java/gr/europeandynamics/xmlmanagement/xmlmanipulationservice/XmlManipulator.java
@@ -0,0 +1,11 @@
+
+package gr.europeandynamics.xmlmanagement.xmlmanipulationservice;
+
+
+public interface XmlManipulator {
+    
+     void readXml(String fileName);
+     void writeXml(String fileName, int startChapter,int endChapter);
+     
+    
+}
