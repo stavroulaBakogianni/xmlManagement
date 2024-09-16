90794951fb9ea00ccbd5a2b26547c258ff156856
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-16 21:12:10 +0300

    Update Chapter class, add addParagraph, getNumberofParagraphs methods

--- src/main/java/gr/europeandynamics/xmlmanagement/domain/Chapter.java
+++ src/main/java/gr/europeandynamics/xmlmanagement/domain/Chapter.java
@@ -1,16 +1,26 @@
 package gr.europeandynamics.xmlmanagement.domain;
 
 import jakarta.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class Chapter {
 
-    private List<Paragraph> paragraphs;
+    private List<Paragraph> paragraphs = new ArrayList<>();;
 
     @XmlElement(name = "paragraph")
-    List<Paragraph> getParagraphs() {
+    public List<Paragraph> getParagraphs() {
         return this.paragraphs;
     }
+
+    public void addParagraph(Paragraph paragraph) {
+        paragraphs.add(paragraph);
+    }
+
+    public int getNumberofParagraphs() {
+        return this.paragraphs.size();
+    }
+
 }
