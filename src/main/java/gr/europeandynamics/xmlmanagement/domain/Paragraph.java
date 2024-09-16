99f4cf3d627a2b4b01838678334b5a0411c46acd
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-16 21:12:57 +0300

    Update Paragraph class, add addSentence, getNumberofSentences methods

--- src/main/java/gr/europeandynamics/xmlmanagement/domain/Paragraph.java
+++ src/main/java/gr/europeandynamics/xmlmanagement/domain/Paragraph.java
@@ -1,16 +1,25 @@
 package gr.europeandynamics.xmlmanagement.domain;
 
 import jakarta.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class Paragraph {
 
-    private List<Sentence> sentences;
+    private List<Sentence> sentences = new ArrayList<>();
 
     @XmlElement(name = "sentence")
-    List<Sentence> getSentences() {
+    public List<Sentence> getSentences() {
         return this.sentences;
+    }
+
+    public void addSentence(Sentence sentence) {
+        sentences.add(sentence);
+    }
+
+    public int getNumberofSentences() {
+        return this.sentences.size();
     }
 }
