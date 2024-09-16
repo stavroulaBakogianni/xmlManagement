7168903f1d735bd173d94fcee6fa41ad690db6e2
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-16 21:16:11 +0300

    Update main, call xmlManipulator.readXml, xmlManipulator.writeXml

--- src/main/java/gr/europeandynamics/xmlmanagement/XmlManagement.java
+++ src/main/java/gr/europeandynamics/xmlmanagement/XmlManagement.java
@@ -1,6 +1,8 @@
 package gr.europeandynamics.xmlmanagement;
 
 import gr.europeandynamics.xmlmanagement.fileconvertorservice.FileConvertorImpl;
+import gr.europeandynamics.xmlmanagement.xmlmanipulationservice.XmlManipulator;
+import gr.europeandynamics.xmlmanagement.xmlmanipulationservice.XmlManipulatorImpl;
 import gr.europeandynamics.xmlmanagement.xmlmanipulationservice.XmlValidator;
 import gr.europeandynamics.xmlmanagement.xmlmanipulationservice.XsdGenerator;
 
@@ -9,13 +11,19 @@ import java.io.IOException;
 public class XmlManagement {
 
     public static void main(String[] args) throws IOException {
-
+        final String txtFileName = "txt_files/sample-lorem-ipsum-text-file.txt";
+        final String xmlFileName = "xml_files/book.xml";
+        final String xsdFileName = "xml_files/book-schema.xsd";
+        final String xmlFragmentFileName = "xml_files/fragment-chapters.xml";
         FileConvertorImpl fileConvertor = new FileConvertorImpl();
 
-        fileConvertor.convertFile("txt_files/sample-lorem-ipsum-text-file.txt", "xml_files/book.xml");
-        final String xsdFileName = "xml_files/book-schema.xsd";
+        fileConvertor.convertFile(txtFileName, xmlFileName);
+
         XsdGenerator.xsdGenerator(xsdFileName);
-        final String xmlFileName = "xml_files/book.xml";
         XmlValidator.xmlValidator(xmlFileName, xsdFileName);
+        XmlManipulator xmlManipulator = new XmlManipulatorImpl();
+        xmlManipulator.readXml(xmlFileName);
+        xmlManipulator.writeXml(xmlFragmentFileName, 5, 8);
     }
+
 }
