3fedcb97110e2fd6785e084d587014654fe3981b
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-17 23:07:24 +0300

    Add calculateStatistics method to the Interface

--- src/main/java/gr/europeandynamics/xmlmanagement/xmlmanipulationservice/XmlManipulator.java
+++ src/main/java/gr/europeandynamics/xmlmanagement/xmlmanipulationservice/XmlManipulator.java
@@ -1,11 +1,11 @@
-
 package gr.europeandynamics.xmlmanagement.xmlmanipulationservice;
 
-
 public interface XmlManipulator {
-    
-     void readXml(String fileName);
-     void writeXml(String fileName, int startChapter,int endChapter);
-     
-    
+
+    void readXml(String fileName);
+
+    void writeXml(String fileName, int startChapter, int endChapter);
+
+    void calculateStatistics();
+
 }
